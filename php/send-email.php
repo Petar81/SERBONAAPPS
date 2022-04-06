60eac0b4a8c80bb3347d3fea3eaa1bcee385a15b
Petar <baki_pfc@yahoo.com>
2022-04-07 01:37:18 +0200

    add logic to send-email.php

---
+++ php/send-email.php
@@ -0,0 +1,11 @@
+<?php
+// get the POST parameter from form
+$firstName = $_POST["firstName"];
+$lasttName = $_POST["lasttName"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+
+
+// Output "no suggestion" if no hint was found or output correct values
+echo "Hi" + $firstName;
+?>
