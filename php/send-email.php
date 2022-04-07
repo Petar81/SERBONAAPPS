833501584dbea91ded446bfb7a4e5c1dd82bd005
Petar <baki_pfc@yahoo.com>
2022-04-07 18:34:08 +0200

    define more POST variables and input them into the mail body

--- php/send-email.php
+++ php/send-email.php
@@ -1,11 +1,50 @@
 <?php
 // get the POST parameter from form
 $firstName = $_POST["firstName"];
-$lasttName = $_POST["lasttName"];
+$lastName = $_POST["lastName"];
 $email = $_POST["email"];
 $message = $_POST["message"];
+$phone = $_POST["phone"];
+$company = $_POST["company"];
+$country = $_POST["country"];
+$address = $_POST["address"];
+$zip = $_POST["zip"];
 
 
-// Output "no suggestion" if no hint was found or output correct values
-echo "Hi" + $firstName;
+
+$to = $email . ", serbonaapps@gmail.com, info@serbonaapps.com";
+$subject = "Serbona Apps has been inquired by ". $firstName . " " . $lastName ;
+
+$email_message = "
+<html>
+<head>
+<title>Serbona Apps Message Center</title>
+</head>
+<body>
+<h1>Serbona Apps Message Center</h1>
+<h2>Your email card:</h2>
+<h3>Full name: " . $firstName . " " . $lastName . "</h3>
+<h3>Email: " . $email . "</h3>
+<h3>Phone: " . $phone . "</h3>
+<h3>Company: " . $company . "</h3>
+<h3>Address: " . $address . "</h3>
+<h3>ZIP: " . $zip . "</h3>
+<h3>Country: " . $country . "</h3>
+<h4>Message: ". $message . "</h4>
+</body>
+</html>
+";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+$headers .= 'From: <noreply@serbonaapps.com>' . "\r\n";
+
+
+if(mail($to,$subject,$email_message,$headers)){
+    // Output "no suggestion" if no hint was found or output correct values
+    echo "Hi " . $firstName . ", your message is on the way to our inbox. <br /> Thanks for contacting us, we'll reply shortly.";
+};
+
+
 ?>
